677098dc208c028fe1b277f87bb54c13436c5249
lilykaw <lily.kawaoto@gmail.com>
2021-11-21 23:10:17 -0500

    added BOW column to dataframe

--- a_1_2_util.py
+++ a_1_2_util.py
@@ -6,6 +6,7 @@ LastEditTime: 2021-11-20 14:56:45
 import re
 import string
 import numpy as np
+import treetaggerwrapper
 
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
@@ -19,6 +20,10 @@ class TweetsData(object):
         
         # preprocess the tweets
         self.df['CleanTweet'] = self.preprocess()
+
+        # added column: new vocab only consists of Nouns, Adjectives, and Verbs
+        # basically, filtered sentences of 'CleanTweet'
+        self.df['BOW'] = self.get_nav()
         
     def preprocess(self): 
         # 1. remove the punctuations 
@@ -55,3 +60,32 @@ class TweetsData(object):
         X = target_df['CleanTweet'].to_list()
         Y = target_df['Stance'].to_list()
         return X, Y
+
+    def get_data_of_target2(self, target): 
+        # filter the data by target
+        target_df = self.df[self.df['Target']==target]
+        X = target_df['BOW'].to_list()
+        Y = target_df['Stance'].to_list()
+        return X, Y
+
+
+    def get_nav(self):
+        # changes sentences in 'CleanTweet' into only Nouns, Adjectives, & Verbs
+        # note: see treetaggerwrapper manual on @daimrod 's GitHub for TreeTagger directory
+        # # Yuhui: I install the TreeTagger in a different directory:
+          # tagger = treetaggerwrapper.TreeTagger(TAGLANG='en', TAGDIR='/mnt/d/install/treetagger/') 
+        NAV_LIST = ['NP', 'NPS', 'NN', 'NNS', 'JJ', 'JJR', 'JJS', 'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']
+        tagger = treetaggerwrapper.TreeTagger(TAGLANG='en')
+        cleantweets = self.df['CleanTweet'].to_list()
+        
+        pos_tweets = [tagger.tag_text(tweet) for tweet in cleantweets] # tagger format: 'word\tPOS\tlemma'
+        nav = []
+        for tweet in pos_tweets:
+            str = ''
+            for tagged_word in tweet: 
+                word, POS, lemma = tagged_word.split('\t')
+                if POS in NAV_LIST:
+                    str += word + ' '
+            nav.append(str)
+
+        return nav
